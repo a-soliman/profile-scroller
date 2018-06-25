1ee50a84dfd336b72ec3aee1b6d6e1576e906543
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-25 09:04:46 -0700

    coded profileIterator function

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        name: 'John Doe',
+        age: 32,
+        gender: 'male',
+        lookingfor: 'female',
+        location: 'Boston MA',
+        image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    },
+    {
+        name: 'Jen Smith',
+        age: 36,
+        gender: 'female',
+        lookingfor: 'male',
+        location: 'Miami FL',
+        image: 'https://randomuser.me/api/portraits/women/82.jpg'
+    },
+    {
+        name: 'William Johnson',
+        age: 38,
+        gender: 'male',
+        lookingfor: 'female',
+        location: 'Los Angles CA',
+        image: 'https://randomuser.me/api/portraits/men/83.jpg'
+    }
+];
+
+// Profile Iterator
+function profileIterator(profiles) {
+    let nextIndex = 0;
+
+    return {
+        next: function() {
+            return nextIndex < profiles.length ? 
+            { value: profiles[nextIndex++], done: false } :
+            { done: true };
+        }
+    };
+}
